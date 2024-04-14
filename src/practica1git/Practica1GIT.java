4d423767b419c4e851a102bf2206882c0087f40c
soula <soula@LAPTOP-K0TOAHUG>
2024-04-14 23:41:47 +0200

    Actualizamos el código y arreglamos errores

--- src/practica1git/Practica1GIT.java
+++ src/practica1git/Practica1GIT.java
@@ -1,93 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practica1git;
 
 import java.util.Scanner;
 
-/**
- *
- * @author esther
- */
 public class Practica1GIT {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double resultado = 0;
-        double num1;
-        double num2;
-        String operacion;
-        boolean comprobar = false;
-
+       
         do {
-
-            System.out.println("\n Introdueix el primer numero. ");
-            num1 = sc.nextDouble();
-
-            System.out.println("\n Introdueix el primer numero. ");
-            num2 = sc.nextDouble();
-
-            do {
-                System.out.println("\n Operació? (Indica el signe)");
-                System.out.println("+ = sumar \n - = restar \n"
-                        + " x = multiplicar \n / = dividir \n * = elevar primer num al segon num."
-                        + "\n % = residu");
-                operacion = sc.nextLine();
-                if (operacion.equals("+") || operacion.equals("-") || operacion.equals("x")
-                        || operacion.equals("X") || operacion.equals("/") || operacion.equals("%")
-                        || operacion.equals("*")) {
-                    comprobar = true;
-                } else {
-                    comprobar = false;
-                }
-            } while (comprobar != true);
-
-            do {
-                comprobar = true;
-                switch (operacion) {
-                    case "+":
-                        resultado = num2 + num2;
-                        break;
-                    case "-":
-                        resultado = num1 - num2;
-                        break;
-                    case "x":
-                    case "X":
-                        resultado = num1 * num2;
-                        break;
-                    case "/":
-                        if (num2 == 0) {
-                            System.err.println(" Al denominador hi ha un zero \n"
-                                    + "per a  evitar errors coloca un altre valor.");
-                        } else {
-                            resultado = num1 / num2;
-                        }
-                        break;
-                    case "*":
-                        resultado = Math.pow(num1, num1);
-                        break;
-                    case "%":
-                        if (num2 == 0) {
-                            System.err.println(" Al denominador hi ha un zero \n"
-                                    + "per a  evitar errors coloca un altre valor.");
-                        } else {
-                            resultado = num1 % num2;
-                        }
-                        break;
-                }
-
-            } while (comprobar != true);
-
+            System.out.println("\nIntroduce el primer número:");
+            double num1 = obtenerNumero(sc);
+           
+            System.out.println("\nOperación? (+, -, x, /, *, %):");
+            String operacion = obtenerOperacion(sc);
+           
+            System.out.println("\nIntroduce el segundo número:");
+            double num2 = obtenerNumero(sc);
+           
+            double resultado = calcularResultado(num1, num2, operacion);
+           
             System.out.println("\nResultado: " + num1 + " " + operacion + " " + num2 + " = " + resultado);
-
+           
             System.out.println("\n¿Quieres continuar operando? (s/n):");
-
-        } while (sc.nextLine()
-                .equalsIgnoreCase("s"));
+        } while (sc.nextLine().equalsIgnoreCase("s"));
+    }
+   
+    public static double obtenerNumero(Scanner sc) {
+        while (true) {
+            String input = sc.nextLine();
+            if (input.matches("[+-]?[\\d]*[.]?[\\d]+")) {
+                return Double.parseDouble(input);
+            } else {
+                System.err.println("\nError: Ingresa un número válido.");
+            }
+        }
+    }
+   
+    public static String obtenerOperacion(Scanner sc) {
+        while (true) {
+            String operacion = sc.nextLine();
+            if (operacion.matches("[+\\-xX/*%]")) {
+                return operacion;
+            } else {
+                System.err.println("\nError: Ingresa una operación válida (+, -, x, /, *, %).");
+            }
+        }
+    }
+   
+    public static double calcularResultado(double num1, double num2, String operacion) {
+        switch (operacion) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "x":
+            case "X":
+                return num1 * num2;
+            case "/":
+                if (num2 == 0) {
+                     System.out.println("Al denominador hi ha un zero per a  evitar errors coloca un altre valor un altre valor.");
+                    return Double.NaN;
+                }
+                return num1 / num2;
+            case "*":
+                return Math.pow(num1, num2);
+            case "%":
+                if (num2 == 0) {
+                    System.out.println("Al denominador hi ha un zero per a  evitar errors coloca un altre valor un altre valor.");
+                   
+                    return Double.NaN;
+                }
+                return num1 % num2;
+            default:
+                System.err.println("\nError: Operación no válida.");
+                return Double.NaN;
+        }
     }
 }
